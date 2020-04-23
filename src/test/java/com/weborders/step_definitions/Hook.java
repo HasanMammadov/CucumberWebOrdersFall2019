a1339d825fe8caccab88a809e66a720834a5f816
hasan <hasanmamedoff@gmail.com>
2020-04-23 03:24:18 -0400

    Apache POI added

--- src/test/java/com/weborders/step_definitions/Hook.java
+++ src/test/java/com/weborders/step_definitions/Hook.java
@@ -1,4 +1,33 @@
 package com.weborders.step_definitions;
 
+import com.weborders.utilities.ConfigurationReader;
+import com.weborders.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 public class Hook {
+    @Before()             // diqqet ele hardan ipmort olub. io.cucumber.java.Before;
+    public void setup(Scenario scenario){
+        System.out.println(":::: Starting test automation ::::");
+        System.out.println("Browser type :: " + ConfigurationReader.getProperty("browser"));
+        System.out.println("Environment :: " + ConfigurationReader.getProperty("url"));
+        System.out.println("Test scenario :: " + scenario.getName());
+        Driver.getDriver().manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        //how to check if scenario failed
+        if (scenario.isFailed()){
+            TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+            byte [] image = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+            //attach screenshot to the report
+            scenario.embed(image,"image/png",scenario.getName());
+        }
+        System.out.println("Test clean up");
+        Driver.closeDriver();
+    }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,2 +1,2 @@
-Feature:
+Feature: Login
   Scenario:
